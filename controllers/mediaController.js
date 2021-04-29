daf9f126ca98088bc14e562d84ca76967cdf0eeb
Helen Regula <margaretregula@MacBook-Pro.local>
2021-04-29 15:31:19 -0400

    Fixed Update Bug and Refactored Add and Get Media

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -4,84 +4,109 @@ const { User } = require('../models/mainModels.js');
 // object contains all mediaController middleware
 const mediaController = {};
 
-//TODO: Refactor with async/await
 // middleware to get entire media profile of user
-mediaController.getMedia = (req, res, next) => {
+mediaController.getMedia = async (req, res, next) => {
+  //userId will be sent to the backend via query parameters
+  const { userId } = req.query;
   // find all media catalog
-  console.log(req.params, req.query);
-  User.findOne({_id: req.query.userId})
-    .exec()
-    // response is the found User
-    .then((response) => {
-      // console.log to see if fired
-      console.log('mediaController getMedia fired');
-      // if no user found
-      if (!response) {
-        // store an empty object in local memory for front end
-        res.locals.media = {}; //shouldnt this be an empty array
-        return next();
-      }
-      // store in local memory the User.media
-      res.locals.media = response.media;
+  
+  try {
+  const userDoc = await User.findOne({_id: userId}).exec()
+    // if no user was found, return an empty media object
+    if (!userDoc) {
+      res.locals.media = {};
       return next();
+    }
+    
+    res.locals.media= userDoc.media;
+      
+    return next();
+
+  } catch (err) {
+    return next({
+      log: `User ${userId} does not exist in the database, returned data came back as null`,
+      status: 500,
+      message: `Error Occurred When Finding User in Database, Please input a valid User ID`
     })
-    .catch((error) => {
-      console.log(err.stack);
-      return next('error in getMedia middleware');
-    });
+  }
 };
 
 
 // add a media type to user profile
-mediaController.addMedia = (req, res, next) => {
+mediaController.addMedia = async (req, res, next) => {
   // finds and update media property on user and returns the updated user
-  User.findOneAndUpdate(
-    // filter for the _id
-    {_id: req.query.userId},
-    // $push is a mongoDb method to add into media Array
-    {$push: {media: req.body}},
-    // this property shows the new updated version
-    {new: true}
-  )
-    .then((user) => {
+  const { userId } = req.query;
+
+  try {
+
+    const userMediaArray = await User.findOneAndUpdate(
+      // filter for the _id
+      { _id: userId },
+      // $push is a mongoDb method to add into media Array
+      { $push: { media: req.body } },
+      // this property shows the new updated version
+      { new: true, projection: "media"} 
+      
+    ).exec();
+
+    if (!userMediaArray){
+      return next({
+        log: `User ${userId} does not exist in the database, returned data came back as null`,
+        status: 400,
+        message: `Error Occurred When Finding User in Database, Please input a valid User ID`
+      })
+    }
+
       // store in local memory that last media item just added
-      res.locals.media = user.media[user.media.length - 1];
+      res.locals.media = userMediaArray.media[userMediaArray.media.length - 1];
       return next();
-    })
-    .catch((err) => {
-      console.log(err.stack);
-      return next('error in addMedia middleware');
-    });
+
+  } catch (err) {
+    return next(
+      {
+        log: `Couldn't Update Media for the User: ${err}`,
+         message: {error: `Error Occured When Adding Media to the user in the User Collection for user ${userId}`}
+      })
+  }
 };
 
 // update the entry of a specific media type
 mediaController.updateMedia = async (req, res, next) => {
   
   const { userId } = req.params;
-  const { _id: mediaId } = req.body;
+  const { _id: mediaId, title, type, currentStatus } = req.body;
+
 
   try {
-    
+
     const updateUserMediaObj = await User.findOneAndUpdate(
-      //filter out the user in the user collection
-      { _id: userId }, 
-      //update the media field in User document with the received data from the client
-      { $set: { media : req.body }}, 
-      //projection allows for a specific part of the document to be returned, then inside the media array in the user doc, return only the media object that matches the mediaID, the returned document will be the user id and a media with one value in the array of the updated media
-      { new: true, projection: { media: {$elemMatch: {_id: mediaId} } } }
-    ).exec();
-    
-    //if user isn't found- return error status of 404
-    if (updateUserMediaObj === null) {
+          //filter out the user in the user collection, and finds the specific media within the media array of the user document
+          { _id: userId, "media._id": mediaId }, 
+          //updates all the fields in the object of the mediaId
+          { $set:
+            { 
+              "media.$.title" : title,
+              "media.$.type": type,
+              "media.$.currentStatus": currentStatus,
+            }
+          }, 
+          //projection allows for a specific part of the document to be returned, then inside the media array in the user doc, return only the media object that matches the mediaID, the returned document will be the user id and a media with one value in the array of the updated media
+          { new: true, projection: { media: {$elemMatch: {_id: mediaId} } }  }
+        ).exec();
+   
+        
+
+    if (!updateUserMediaObj) {
       return next({
-        log: `User ${userId} does not exist in the database, returned data came back as null`,
-        status: 404,
+        log: `User ${userId} does not exist in the database`,
+        status: 400,
         message: `Error Occurred When Finding User in Database, Please input a valid User ID`
       })
     }
 
     //take the returned data from the media array and save it to the response body
     res.locals.media = updateUserMediaObj.media[0];
+
     return next();
 
   } catch (err) {
@@ -128,3 +153,25 @@ mediaController.deleteMedia = async (req, res, next) => {
 
 // exports mediaController object to server
 module.exports = mediaController;
+
+// try {
+    
+//   const updateUserMediaObj = await User.findOneAndUpdate(
+//     //filter out the user in the user collection
+//     { _id: userId }, 
+//     //update the media field in User document with the received data from the client
+//     { $set: { media : req.body }}, 
+//     //projection allows for a specific part of the document to be returned, then inside the media array in the user doc, return only the media object that matches the mediaID, the returned document will be the user id and a media with one value in the array of the updated media
+//     { new: true, projection: { media: {$elemMatch: {_id: mediaId} } } }
+//   ).exec();
+  
+//   //if user isn't found- return error status of 404
+//   if (updateUserMediaObj === null) {
+//     return next({
+//       log: `User ${userId} does not exist in the database, returned data came back as null`,
+//       status: 404,
+//       message: `Error Occurred When Finding User in Database, Please input a valid User ID`
+//     })
+//   }
+
+//   
